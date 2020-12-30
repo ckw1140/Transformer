337325b3c2f37c665b52294a61700e08f3ac1e00
기원최 <giwonchoe@giwonchoeui-MacBookPro.local>
2020-12-30 11:40:24 +0900

    style: 일관된 패키지 사용

--- model/utils.py
+++ model/utils.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 
 def gelu(x):
     cdf = 0.5 * (
-        1.0 + torch.tanh((math.sqrt(2 / np.pi) * (x + 0.044715 * torch.pow(x, 3))))
+        1.0 + torch.tanh((math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
     )
     return x * cdf
 
