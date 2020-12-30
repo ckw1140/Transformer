d5eab1584b665686060e632537dca33173224763
기원최 <giwonchoe@giwonchoeui-MacBookPro.local>
2020-12-30 13:46:46 +0900

    chore: 불필요한 주석 삭제

--- model/transformer.py
+++ model/transformer.py
@@ -1,10 +1,3 @@
-"""
-https://paul-hyun.github.io/transformer-01/
-https://github.com/paul-hyun/transformer-evolution/blob/master/transformer/model.py
-
-위의 블로그와 코드를 참고하여 구현한 코드입니다.
-"""
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
